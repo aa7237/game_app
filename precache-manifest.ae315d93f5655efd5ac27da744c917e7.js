8ab4c87678fb7f6370a9019f525b6c28633238c5
Petrucio0515 <45837396+Petrucio0515@users.noreply.github.com>
2019-09-21 21:54:28 +0900

    Updates

---
+++ precache-manifest.ae315d93f5655efd5ac27da744c917e7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f123e537fb8328fea83dd65f47506f92",
+    "url": "/game_app/index.html"
+  },
+  {
+    "revision": "cb55216a70e7877e3322",
+    "url": "/game_app/static/css/main.c18ee8fc.chunk.css"
+  },
+  {
+    "revision": "176c30e0140817c4dc60",
+    "url": "/game_app/static/js/2.13a082d6.chunk.js"
+  },
+  {
+    "revision": "cb55216a70e7877e3322",
+    "url": "/game_app/static/js/main.f6af5341.chunk.js"
+  },
+  {
+    "revision": "c656a99e763e9d6580ba",
+    "url": "/game_app/static/js/runtime-main.a0267945.js"
+  }
+]);
